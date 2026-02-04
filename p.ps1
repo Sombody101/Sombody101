e698eaf49fa6080a81720c838e940d44359efc84
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2026-02-04 10:15:16 -0700

    Refactor stream reading into a reusable function

--- p.ps1
+++ p.ps1
@@ -1,19 +1,22 @@
- $request = [System.Net.HttpWebRequest]::Create("http://parrot.live");
- $response = $request.GetResponse();
- $receiveStream = $response.GetResponseStream();
- $readStream = [System.IO.StreamReader]::new($receiveStream);
-
+function get_stream {
+   $request = [System.Net.HttpWebRequest]::Create("http://parrot.live");
+   $response = $request.GetResponse();
+   $receiveStream = $response.GetResponseStream();
+   return [System.IO.StreamReader]::new($receiveStream);
+}
+ 
+ $readStream = get_stream
  [console]::TreatControlCAsInput = $true;
 
 while ($true){
  $initialForegroundColor = [Console]::ForegroundColor;
- while ($line = $readStream.ReadLine()) {
-   [Console]::WriteLine($line);
+ try {
+   while ($line = $readStream.ReadLine()) {
+     [Console]::WriteLine($line);
+   }
+ }
+ catch 
+ {
+   $readStream = get_stream
  }
 }
-
- $readStream.Close();
- $receiveStream.Close();
- $request.Abort();
- [console]::TreatControlCAsInput = $false;
- [Console]::ForegroundColor = $initialForegroundColor;
